4d6b4c1fd991ad1df7f63610f56d02c3506e055b
Mingk42 <xoals123456t@gmail.com>
2024-09-25 12:13:56 +0900

    except invert

--- src/mnist/pred.py
+++ src/mnist/pred.py
@@ -13,7 +13,7 @@ def preprocess_image(image_path):
     img = img.resize((28, 28))  # 크기 조정
 
     # 흑백 반전
-    img = 255 - np.array(img)  # 흑백 반전
+    # img = 255 - np.array(img)  # 흑백 반전
 
     img = np.array(img)
     img = img.reshape(1, 28, 28, 1)  # 모델 입력 형태에 맞게 변형
